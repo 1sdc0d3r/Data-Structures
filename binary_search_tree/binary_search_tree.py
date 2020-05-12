4e8c2130c2a5bb810a306f04f2256eda24ae675d
Braden <fish0859@gmail.com>
2020-05-12 11:33:46 -0600

    finished stretch goals on binary_search_tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -16,23 +16,36 @@ class BinarySearchTree:
         self.value = value
         self.left = None
         self.right = None
+        self.height = 1
+
+    def __str__(self):
+        return f"Height: {self.height}"
+
+    # def balance(self):
+    #     if self.heightL == self.heightR:
+    #         self.balanced = True
+    #     else:
+    #         self.balanced = False
 
     # Insert the given value into the tree
     def insert(self, value):
         node = BinarySearchTree(value)
-        if not self.value:
-            self.value = value
-        else:
-            if value < self.value:
-                if self.left:
-                    self.left.insert(value)
-                else:
-                    self.left = node
+        self.height += 1
+        # * You don't need to check if empty because the tree will always be initializer with a single value
+        # if not self.value:
+        #     self.value = value
+        # else:
+        if value < self.value:
+            if self.left:
+                self.left.insert(value)
             else:
-                if self.right:
-                    self.right.insert(value)
-                else:
-                    self.right = node
+                self.left = node
+        else:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = node
+        # self.balance()
 
     # Return True if the tree contains the value
     # False if it does not
@@ -61,7 +74,8 @@ class BinarySearchTree:
         else:
             return self.value
 
-    # Call the function `fn` on the value of each node
+    # Call the function `fn` on the value of each node use recursive or iterative
+    # * pre-order traversal
     def for_each(self, fn):
         fn(self.value)
         if self.left:
@@ -71,8 +85,8 @@ class BinarySearchTree:
 
     # Part 2 -----------------------
 
-            # Print all the values in order from low to high
-            # ? Hint:  Use a recursive, depth first traversal
+    # Print all the values in order from low to high
+    # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
         orderedList = []
         def cb(x): return orderedList.append(x)
@@ -81,32 +95,58 @@ class BinarySearchTree:
         for num in orderedList:
             print(num)
 
-    # ? Print the value of every node, starting with the given node, in an iterative breadth first traversal
-    # todo
-    def bft_print(self, node):
-        orderedSet = set()
-        def cb(x): return orderedSet.add(x)
-        node.for_each(cb)
-        orderedSet = sorted(orderedSet)
-        return orderedSet
-        # for num in orderedSet:
-        #     print(num)
+    # Print the value of every node, starting with the given node, in an iterative breadth first traversal
 
-    # ? Print the value of every node, starting with the given node, in an iterative depth first traversal
+    # ! BFT is like a queue
+    def bft_print(self, node):
+        # Create Queue
+        # add root to queue
+        queue = [node]
+        # while queue is not empty
+        while(queue):
+            # node = pop head of queue
+            node = queue.pop(0)
+            # do the thing (print)
+            print(node.value)
+            # add children of node to queue
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+
+    #  Print the value of every node, starting with the given node, in an iterative depth first traversal
+
+    # ! DFT is like a stack
     def dft_print(self, node):
-        orderedList = []
-        def cb(x): return orderedList.append(x)
-        node.for_each(cb)
-        for num in orderedList:
-            print(num)
+        # Create stack
+        # add root to stack
+        stack = [node]
+        # while stack is not empty
+        while stack:
+            # node = pop head of stack
+            node = stack.pop()
+            # do the thing (print)
+            print(node.value)
+            # add children of node to stack
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
 
     #! Stretch Goals -------------------------
     # Note: Research may be required
-
     # Print Pre-order recursive DFT
     def pre_order_dft(self, node):
-        pass
+        print(node.value)
+        if node.left:
+            self.pre_order_dft(node.left)
+        if node.right:
+            self.pre_order_dft(node.right)
 
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
-        pass
+        if node.left:
+            self.post_order_dft(node.left)
+        if node.right:
+            self.post_order_dft(node.right)
+        print(node.value)
--- binary_search_tree/tempCodeRunnerFile.py
+++ binary_search_tree/tempCodeRunnerFile.py
@@ -1,3 +1,4 @@
-    def test_for_each(self):
-        arr = []
-        def cb(x): return arr.append(x)
+    # sys.stdout = io.StringIO()
+        # self.bst.post_order_dft(self.bst)
+        # output = sys.stdout.getvalue()
+        # self.assertEqual(output, "2\n4\n3\n6\n7\n5\n8\n1\n")
